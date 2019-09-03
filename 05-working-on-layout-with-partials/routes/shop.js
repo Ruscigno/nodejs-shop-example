f4749f214ee417a0f73ca2aacea188375d252c3e
Sander Ruscigno <ruscigno@gmail.com>
2019-09-03 17:11:48 -0300

    feature: MVC

--- 05-working-on-layout-with-partials/routes/shop.js
+++ 05-working-on-layout-with-partials/routes/shop.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const produtcsController = require('../controllers/products');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', produtcsController.getProducts);
+router.get('/', productsController.getProducts);
 
 module.exports = router;
